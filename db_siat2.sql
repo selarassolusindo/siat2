ef04feee114947688fd7a355e0afbf0f6092835d
selarassolusindo@fujitsuLH531 <project.selusin@gmail.com>
2021-02-20 19:16:14 +0700

    .

--- db_siat2.sql
+++ db_siat2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2021 at 01:10 PM
+-- Generation Time: Feb 20, 2021 at 01:15 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
