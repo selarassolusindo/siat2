799b5f418499af0a80b66fe80f14f446e0d896b5
selarassolusindo_on_lenovo_i3 <project.selusin@gmail.com>
2021-04-30 01:49:43 +0700

    .

--- harviacode/core/create_view_list.php
+++ harviacode/core/create_view_list.php
@@ -75,7 +75,7 @@ $string .=  "\n\t\t\t</tr>
         </table>
         <div class=\"row\">
             <div class=\"col-md-6\">
-                <a href=\"#\" class=\"btn btn-primary\">Total Data : <?php echo \$total_rows ?></a>";
+                <!-- <a href=\"#\" class=\"btn btn-primary\">Total Data : <?php echo \$total_rows ?></a>";
 if ($export_excel == '1') {
     $string .= "\n\t\t\t\t<?php echo anchor(site_url('".$c_url."/excel'), 'Excel', 'class=\"btn btn-primary\"'); ?>";
 }
@@ -83,7 +83,7 @@ if ($export_word == '1') {
     $string .= "\n\t\t\t\t<?php echo anchor(site_url('".$c_url."/word'), 'Word', 'class=\"btn btn-primary\"'); ?>";
 }
 if ($export_pdf == '1') {
-    $string .= "\n\t\t\t\t<?php echo anchor(site_url('".$c_url."/pdf'), 'PDF', 'class=\"btn btn-primary\"'); ?>";
+    $string .= "\n\t\t\t\t<?php echo anchor(site_url('".$c_url."/pdf'), 'PDF', 'class=\"btn btn-primary\"'); ?> -->";
 }
 $string .= "\n\t\t\t</div>
             <div class=\"col-md-6 text-right\">
